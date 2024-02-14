e6b434dc9a6b91008828a41ad609249c4913e9e3
ballir27 <ballir2727@gmail.com>
2024-02-13 23:21:34 -0600

    Through Q6

--- AB-lahman-baseball.sql
+++ AB-lahman-baseball.sql
@@ -93,5 +93,70 @@ ORDER BY wins;
 -- The smallest number of wins for a team that won the world series was the LA Dodgers with just 63 wins in 1981, but this was because of the 1981 baseball players' strike.
 -- Besides 1981, the smallest number of wins for a team that won the world series was the St. Louis Cardinals with 83 wins in 2006.
 
+WITH most_win_team AS (
+	SELECT yearid, name, most_wins
+	FROM teams
+	INNER JOIN(
+		SELECT yearid, MAX(W) AS most_wins
+		FROM teams
+		GROUP BY yearid)
+	USING(yearid)
+	WHERE most_wins = W
+),
+
+ws_win_team AS (
+	SELECT yearid, name, WSwin
+	FROM teams
+	WHERE WSwin = 'Y'
+)
+SELECT number_ws_winners_as_most_winners, 
+	ROUND(number_ws_winners_as_most_winners*1.0/number_of_distinct_years,3) AS pct_ws_winners_as_most_winners
+FROM(
+SELECT
+	COUNT(DISTINCT(yearid)) AS number_of_distinct_years,
+	COUNT(CASE WHEN m.name = w.name THEN 1.0 END) AS number_ws_winners_as_most_winners
+FROM most_win_team AS m
+INNER JOIN ws_win_team AS w
+USING(yearid)
+WHERE yearid BETWEEN 1970 AND 2016)
+-- Only 12 teams between 1970 and 2016 won the most games in the season and also won the world series.
+-- This means only 26.1% of world series winners also won the most games in the same season.
+
 -- 6. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)?
 -- Give their full name and the teams that they were managing when they won the award.
+WITH AL_TSN_ManagerotY AS (
+SELECT playerid, name AS ALteam_name, yearid, awardsmanagers.lgid
+FROM awardsmanagers
+INNER JOIN managers
+USING(playerid, yearid)
+INNER JOIN teams
+USING(teamid, yearid)
+WHERE awardid = 'TSN Manager of the Year'
+	AND awardsmanagers.lgid = 'AL'),
+
+NL_TSN_ManagerotY AS (
+SELECT playerid, name AS NLteam_name, yearid, awardsmanagers.lgid
+FROM awardsmanagers
+INNER JOIN managers
+USING(playerid, yearid)
+INNER JOIN teams
+USING(teamid, yearid)
+WHERE awardid = 'TSN Manager of the Year'
+	AND awardsmanagers.lgid = 'NL')
+
+SELECT DISTINCT namefirst,
+	namelast,
+	ALteam_name,
+	NLteam_name
+FROM AL_TSN_ManagerotY
+INNER JOIN NL_TSN_ManagerotY
+USING(playerid)
+INNER JOIN people
+USING(playerid);
+-- Davey Johnson won TSN Manager of the Year while managing both the Baltimore Orioles (AL) and the Washington Nationals (NL)
+-- Jim Leyland won TSN Manager of the Year while managing both the Detroit Tigers (AL) and the Pittsburgh Pirates (NL)
+
+-- 7. Which pitcher was the least efficient in 2016 in terms of salary / strikeouts?
+-- Only consider pitchers who started at least 10 games (across all teams).
+-- Note that pitchers often play for more than one team in a season, so be sure that you are counting all stats for each player.
+
